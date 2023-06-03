96d34877e4454442830e446e789b639966eecc9c
dogukanakkan <dogukanakkan3@gmail.com>
2023-06-03 14:11:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin: ");
+        int sayi = scanner.nextInt();
+
+        int toplam = 0;
+        int tempSayi = sayi;
+
+        while (tempSayi != 0) {
+            int basamak = tempSayi % 10;
+            toplam += basamak;
+            tempSayi /= 10;
+        }
+
+        System.out.println("Basamaklar toplamı: " + toplam);
+    }
+}
